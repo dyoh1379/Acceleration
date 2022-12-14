49f08eca8b35d196b22c8debde16dabfebb3ba59
dyoh1 <dyoh1379@gmail.com>
2022-12-15 01:10:25 +0900

    ✨ config.yml 추가

--- src/main/java/kr/dyoh1379/acceleration/Main.java
+++ src/main/java/kr/dyoh1379/acceleration/Main.java
@@ -1,13 +1,51 @@
 package kr.dyoh1379.acceleration;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        FileConfiguration config = this.getConfig();
+        config.addDefault("Game", false);
+        config.options().copyDefaults(true);
+        saveConfig();
+
+        getServer().getPluginCommand("Game").setExecutor(new Command());
+        getServer().getPluginCommand("Game").setTabCompleter(new Command());
+
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+
+        Scoreboard scoreboard = getServer().getScoreboardManager().getMainScoreboard();
+        if(scoreboard.getObjective("Acceleration") == null) {
+            scoreboard.registerNewObjective("Acceleration", "dummy");
+        }
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                for(Player p : Bukkit.getOnlinePlayers()) {
+                    if(config.getBoolean("Game")) {
+                        Score score = p.getScoreboard().getObjective("Acceleration").getScore(p.getName());
+
+                        p.sendActionBar("당신의 속도: " + ChatColor.AQUA + score.getScore());
+
+                        if(score.getScore() != 0) {
+                            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 2, score.getScore() - 1));
+                        }
+                    }
+                }
+            }
+        },0 ,1L);
     }
 
     @Override
