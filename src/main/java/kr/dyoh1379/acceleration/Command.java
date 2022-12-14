5a5d96f47734eba9078b08f18c19fafaa2c5b077
dyoh1 <dyoh1379@gmail.com>
2022-12-15 01:09:09 +0900

    📜 기초 작업 완료

--- src/main/java/kr/dyoh1379/acceleration/Command.java
+++ src/main/java/kr/dyoh1379/acceleration/Command.java
@@ -72,7 +72,6 @@ public class Command implements CommandExecutor, TabCompleter {
                     ->
                     ->
                     -> * 만들기 귀찮다. 시험 끝나고 할까
-
             }
             */
 
