104df229e67cf61b2de1046b3f09b5ef5174614f
dyoh1 <dyoh1379@gmail.com>
2022-12-20 22:51:01 +0900

    🔖 코드 구조 변경

--- src/main/java/kr/dyoh1379/acceleration/Command.java
+++ src/main/java/kr/dyoh1379/acceleration/Command.java
@@ -26,95 +26,146 @@ public class Command implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
         if("Game".equalsIgnoreCase(command.getName())) {
-            if (args.length == 1) {
-                if(sender.isOp()) {
 
-                    if("help".equalsIgnoreCase(args[0])) {
-                        sender.sendMessage(ChatColor.BOLD + "/Game start " + ChatColor.RESET + "게임을 시작합니다");
-                        sender.sendMessage(ChatColor.BOLD + "/Game stop " + ChatColor.RESET + "게임을 종료합니다");
-                        sender.sendMessage(ChatColor.BOLD + "/Game reset " + ChatColor.RESET + "게임 정보를 초기화합니다");
-                        sender.sendMessage("");
-                        sender.sendMessage(ChatColor.BOLD + "Made by. " + ChatColor.RESET + "괴다");
-
-                    }
-
-                    if("start".equalsIgnoreCase(args[0])) {
-                        sender.sendMessage("게임 시작!");
-
-                        config.set("Game", true);
-                        plugin.saveConfig();
-                    }
-
-                    else if("stop".equalsIgnoreCase(args[0])) {
-                        sender.sendMessage("게임 종료");
-
-                        config.set("Game", false);
-                        plugin.saveConfig();
-                    }
-
-                    else if("reset".equalsIgnoreCase(args[0])) {
-                        sender.sendMessage("게임 초기화!");
-
-                        for(Player p : Bukkit.getOnlinePlayers()) {
-                            Score score = p.getScoreboard().getObjective("Acceleration").getScore(p.getName());
-                            score.resetScore();
-                        }
-                    }
-
-                    else {
-                        sender.sendMessage("잘못된 인수 입니다");
-                    }
-
-                }
-
-                else {
-                    sender.sendMessage(Bukkit.getPermissionMessage().replace("&", "§"));
-                }
+            if(!sender.isOp()) {
+                sender.sendMessage(Bukkit.getPermissionMessage().replace("&", "§"));
+                return false;
             }
 
-            /*
-            else if(args.length == 2) {
-                TODO: 'KeepSpeed <True/False>' 인수를 추가하여 사망 시 기존 속도가 유지되는지 결정 할 수 있도록 해야 함.
-                    -> config 에 'KeepSpeed' (Boolean) 항목을 추가해야 함.
-                    -> 'KeepSpeed' 가 'False' 일 때, 해당 유저의 Scoreboard 를 초기화 시킴으로써 속도가 초기화 되도록 할 수 있다.
-                    ->
-                    ->
-                    -> * 만들기 귀찮다. 시험 끝나고 할까
-            }
-            */
+            switch (args.length) {
 
-            else {
-                sender.sendMessage("Usage: /Game <Argument>");
+                case 1:
+                    switch (args[0].toLowerCase()) {
+
+                        case "help":
+                            sender.sendMessage(ChatColor.BOLD + "/Game start " + ChatColor.RESET + "게임을 시작합니다");
+                            sender.sendMessage(ChatColor.BOLD + "/Game stop " + ChatColor.RESET + "게임을 종료합니다");
+                            sender.sendMessage(ChatColor.BOLD + "/Game reset " + ChatColor.RESET + "게임 정보를 초기화합니다");
+                            sender.sendMessage(ChatColor.BOLD + "/Game KeepInfo <Boolean> " + ChatColor.RESET + "사망 시 정보가 유지되는지 설정합니다");
+                            sender.sendMessage("");
+                            sender.sendMessage(ChatColor.BOLD + "Made by. " + ChatColor.RESET + "괴다");
+                            break;
+
+                        case "start":
+
+                            if (config.getBoolean("Game")) {
+                                sender.sendMessage("이미 게임이 시작되었습니다");
+                                return false;
+                            }
+
+                            sender.sendMessage("게임 시작!");
+
+                            config.set("Game", true);
+                            plugin.saveConfig();
+                            break;
+
+                        case "stop":
+                            if (!config.getBoolean("Game")) {
+                                sender.sendMessage("이미 게임이 종료되었습니다");
+                                return false;
+                            }
+
+                            sender.sendMessage("게임 종료");
+
+                            config.set("Game", false);
+                            plugin.saveConfig();
+                            break;
+
+                        case "reset":
+                            sender.sendMessage("게임 초기화!");
+
+                            for(Player p : Bukkit.getOnlinePlayers()) {
+                                Score score = p.getScoreboard().getObjective("Acceleration").getScore(p.getName());
+                                score.resetScore();
+                            }
+                            break;
+
+                        case "keepinfo":
+                            sender.sendMessage("KeepInfo= " + config.getBoolean("KeepInfo"));
+                            break;
+
+                        default:
+                            sender.sendMessage("잘못된 인수 입니다");
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    switch (args[0]) {
+                        case "keepinfo":
+                            if(!isBoolean(args[1])) {
+                                sender.sendMessage("잘못된 인수 입니다");
+                                return false;
+                            }
+
+                            if(config.getBoolean("KeepInfo") == Boolean.parseBoolean(args[1])) {
+                                sender.sendMessage("이미 KeepInfo 값은 " + config.get("KeepInfo") + " 입니다");
+                                return false;
+                            }
+
+                            config.set("KeepInfo", Boolean.parseBoolean(args[1]));
+                            sender.sendMessage("KeepInfo 값을 " + config.get("KeepInfo") + " 로 설정했습니다");
+                            break;
+
+                        default:
+                            sender.sendMessage("잘못된 인수 입니다");
+                            break;
+                    }
+                    break;
+
+                default:
+                    sender.sendMessage("Usage: /Game <Arguments>");
             }
         }
-
         return false;
     }
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, org.bukkit.command.@NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        List<String> list = Arrays.asList("start", "stop", "reset");
+        List<String> list = Arrays.asList("start", "stop", "reset", "keepinfo");
+        List<String> list2 = Arrays.asList("true", "false");
         String input = args[0].toLowerCase();
 
         List<String> completions = null;
 
-        if(args.length == 1) {
-            for(String s : list) {
-                if(s.startsWith(input)) {
-                    if(completions == null) {
-                        completions = new ArrayList();
+
+        switch (args.length) {
+            case 1:
+                for (String s : list) {
+                    if (s.startsWith(input)) {
+                        if (completions == null) {
+                            completions = new ArrayList();
+                        }
+
+                        completions.add(s);
                     }
-
-                    completions.add(s);
                 }
-            }
+                break;
 
-            if(completions != null) {
-                Collections.sort(completions);
-            }
+            case 2:
+                if ("keepinfo".equals(input)) {
+                    for (String s : list2) {
+                        if (s.startsWith(args[1].toLowerCase())) {
+                            if (completions == null) {
+                                completions = new ArrayList();
+                            }
+
+                            completions.add(s);
+                        }
+                    }
+                }
+                break;
         }
-
         return completions;
+    }
+
+    public boolean isBoolean(String s) {
+
+        if(s.equalsIgnoreCase("true") || s.equalsIgnoreCase("false")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
